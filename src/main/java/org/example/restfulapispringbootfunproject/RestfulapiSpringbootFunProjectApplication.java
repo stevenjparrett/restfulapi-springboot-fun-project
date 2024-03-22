c15a6d8a018ec846f1410043c9c4fab2151fe12d
Steven Parrett <steven.parrett@pagesuite.com>
2024-03-22 13:21:09 +0000

    initial jira commit master

---
+++ src/main/java/org/example/restfulapispringbootfunproject/RestfulapiSpringbootFunProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.restfulapispringbootfunproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiSpringbootFunProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulapiSpringbootFunProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restfulapispringbootfunproject/RestfulapiSpringbootFunProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restfulapispringbootfunproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiSpringbootFunProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
